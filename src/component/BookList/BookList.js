ba55a570d39dd5155f83e84d52bcd639ba0534b3
Giorgi Ediberidze <51026202+edo-edo@users.noreply.github.com>
2020-08-08 19:41:49 +0400

    Update BookList.js
    Add some tag on table

--- src/component/BookList/BookList.js
+++ src/component/BookList/BookList.js
@@ -5,18 +5,22 @@ const bookList = ({books, toggleBook}) => {
     return (
         <div className={Style}>
             <table>
-            <tr>
-            <th>Complate</th>
-            <th>Name of book</th>
-            <th>Author</th>
-            <th>Price</th>
-            <th>Number of pages</th>
-            </tr>
+                <thead>
+                    <tr>
+                    <th>Complate</th>
+                    <th>Name of book</th>
+                    <th>Author</th>
+                    <th>Price</th>
+                    <th>Number of pages</th>
+                    </tr>
+                 </thead>
+            <tbody>
             {
                 books.map(book => {
                     return <Book key={book.id} toggleBook={toggleBook} book={book} />
                 })
             }
+            </tbody>
             </table>
         </div>
     )
